91aac7326bd61cdf01478a135164719d1885be6d
oksyd <oksyd@qamadness.com>
2026-04-20 09:19:07 -0400

    first commit

---
+++ cypress.config.js
@@ -0,0 +1,20 @@
+const {defineConfig} = require("cypress");
+
+module.exports = defineConfig({
+    "cypress-cucumber-preprocessor": {
+        nonGlobalStepDefinitions: false,
+
+        step_definitions: "./cypress/e2e/login/",
+    },
+
+    e2e: {
+        setupNodeEvents(on, config) {
+            return require("./cypress/plugins/index.js")(on, config);
+        },
+
+        specPattern: "cypress/e2e/**/*.feature",
+
+        supportFile: false,
+    },
+});
+
